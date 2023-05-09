d07920c1a219ddc8dfaae8f3a180596b4a05c32c
wangcs <3282418217@qq.com>
2023-05-09 19:16:35 +0800

    美团项目提交

---
+++ vue_meituan/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue_meituan/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <div>仿美团项目</div>
+    <!-- <van-button type="primary">主要按钮</van-button> -->
+    <van-button type="primary">主要按钮</van-button>
+    <!-- <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-a-Group160"></use>
+    </svg> -->
+    <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-tihuanren"></use>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+
+<style lang='less' scoped>
+.hello {
+  font-size: 20px;
+  div {
+    color: red;
+  }
+}
+</style>
